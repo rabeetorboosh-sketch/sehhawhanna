66f71e5611ec1e52b0fa292b42e9933854874339
rabee torboosh <rabeetorboosh@gmail.com>
2025-10-30 10:52:11 -0700

    imp cust

--- database/migrations/2025_09_06_181542_create_products_table.php
+++ database/migrations/2025_09_06_181542_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('item_id');
-            $table->string('code');
+            $table->string('code')->nullable();
             $table->text('description')->nullable();
             $table->unsignedBigInteger('branch_id');
             $table->timestamps();
